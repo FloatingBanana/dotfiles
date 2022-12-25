44d5f10988476c09971c3878191ae80bfb0c51a3
FloatingBanana <thalesmaiademoraes13@gmail.com>
2022-12-25 17:07:16 -0300

    nvim plugins and keybindings

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -1,51 +1,80 @@
 local telescope = require "telescope.builtin"
-
-local opts = { noremap = true, silent = true }
-
--- vim.keymap.set('i', '<C-Delete>', ":dw", opts)
-
--- Telescope
-vim.keymap.set('n', '<leader>ff', '<Cmd>Telescope find_files<CR>', opts)
-vim.keymap.set('n', '<leader>fg', '<Cmd>Telescope find_grep<CR>', opts)
-vim.keymap.set('n', '<leader>fb', '<Cmd>Telescope buffers<CR>', opts)
-
--- Move lines
-vim.keymap.set('n', '<A-Up>', '<Cmd>:move .-2<CR>', opts)
-vim.keymap.set('n', '<A-Down>', '<Cmd>:move .+<CR>1', opts)
-
--- LSP
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
-vim.keymap.set('n', '<leader>lr', telescope.lsp_references, opts)
-vim.keymap.set('n', '<leader>ld', telescope.lsp_definitions, opts)
-
--- Debugger
 local dap = require "dap"
-vim.keymap.set('n', '<leader>dbb', dap.toggle_breakpoint, opts)
-vim.keymap.set('n', '<leader>dc', dap.continue, opts)
-vim.keymap.set('n', '<leader>dso', dap.step_out, opts)
-vim.keymap.set('n', '<leader>dsi', dap.step_into, opts)
-vim.keymap.set('n', '<leader>dsv', dap.step_over, opts)
-vim.keymap.set('n', '<leader>dsb', dap.step_back, opts)
-vim.keymap.set('n', '<leader>di', dap.repl.open, opts)
+local wk = require "which-key"
 
-vim.keymap.set('n', '<leader>dbc', function()
-	vim.fn.inputsave()
-	dap.toggle_breakpoint(vim.fn.input('Condition: ', 'true'))
-	vim.fn.inputsave()
-end, opts)
+local opts = {noremap = true, silent = true}
 
-vim.keymap.set('n', '<leader>dbn', function()
-	vim.fn.inputsave()
-	dap.toggle_breakpoint(nil, vim.fn.input('Hit count: ', 1))
-	vim.fn.inputrestore()
-end, opts)
+vim.keymap.set('i', '<C-d>', "<Cmd>dw<CR>", opts)
+-- Move lines
+vim.keymap.set('i', '<A-Up>', '<Cmd>:move .-2<CR>', opts)
+vim.keymap.set('i', '<A-Down>', '<Cmd>:move .+<CR>', opts)
 
-vim.keymap.set('n', '<leader>dbl', function()
-	vim.fn.inputsave()
-	dap.toggle_breakpoint(nil, nil, vim.fn.input('Log message: ', ''))
-	vim.fn.inputrestore()
-end, opts)
+wk.register({
+	-- Telescope
+	['<leader>f'] = "+Telescope find",
+	['<leader>ff'] = {'<Cmd>Telescope find_files<CR>', "Find file"},
+	['<leader>fg'] = {'<Cmd>Telescope find_grep<CR>', "Grep find"},
+	['<leader>fb'] = {'<Cmd>Telescope buffers<CR>', "Find buffer"},
+
+	-- LSP
+	['<leader>l'] = "+LSP",
+	['<leader>lf'] = {vim.diagnostic.open_float, "Open diagnostic float"},
+	['<leader>lp'] = {vim.diagnostic.goto_prev, "Go to previous"},
+	['<leader>ln'] = {vim.diagnostic.goto_next, "Go to next"},
+	['<leader>lr'] = {telescope.lsp_references, "References"},
+	['<leader>ld'] = {telescope.lsp_definitions, "Definitions"},
+
+	-- Debugger
+	['<leader>d'] = "+Debugger",
+	['<leader>ds'] = "+Step",
+	['<leader>db'] = "+Breakpoints",
+	['<leader>dc'] = {dap.continue, "Continue debugging session"},
+	['<leader>dso'] = {dap.step_out, "Step out"},
+	['<leader>dsi'] = {dap.step_into, "Steo into"},
+	['<leader>dsO'] = {dap.step_over, "Step over"},
+	['<leader>dsb'] = {dap.step_back, "Step back"},
+	['<leader>di'] = {dap.repl.open, "Open REPL"},
+	['<leader>dbb'] = {dap.toggle_breakpoint, "Toggle breakpoint"},
+
+	['<leader>dbc'] = {function ()
+		vim.fn.inputsave()
+		dap.toggle_breakpoint(vim.fn.input('Condition: ', 'true'))
+		vim.fn.inputrestore()
+	end, "Set conditional breakpoint"},
+
+	["<leader>dbn"] = {function ()
+		vim.fn.inputsave()
+		dap.toggle_breakpoint(nil, vim.fn.input('Hit count: ', 1))
+		vim.fn.inputrestore()
+	end, "Set counter breakpoint"},
+
+	["<leader>dbl"] = {function ()
+		vim.fn.inputsave()
+		dap.toggle_breakpoint(nil, nil, vim.fn.input('Log message: ', ''))
+		vim.fn.inputrestore()
+	end, "Set log breakpoint"}
+}, opts)
+
+return {
+	lsp_bufferbindings = function(keyopts)
+		wk.register({
+			['gD'] = {vim.lsp.buf.declaration, "Declarations"},
+			['gd'] = {vim.lsp.buf.definition, "Definitions"},
+			['K'] = {vim.lsp.buf.hover, "Hover"},
+			['gi'] = {vim.lsp.buf.implementation, "Implementation"},
+			['<C-k>'] = {vim.lsp.buf.signature_help, "Signature"},
+			['<space>wa'] = {vim.lsp.buf.add_workspace_folder, "Add workspace folder"},
+			['<space>wr'] = {vim.lsp.buf.remove_workspace_folder, "Remove workspace folder"},
+
+			['<space>wl'] = {function ()
+				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+			end, "List workspace folders"},
+
+			['<space>D'] = {vim.lsp.buf.type_definition, "Type definition"},
+			['<space>rn'] = {vim.lsp.buf.rename, "Rename symbol"},
+			['<space>ca'] = {vim.lsp.buf.code_action, "Code actions"},
+			['gr'] = {vim.lsp.buf.references, "References"},
+			['<space>f'] = {vim.lsp.buf.formatting, "Format code"},
+		}, keyopts)
+	end
+}
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -6,15 +6,19 @@ require("packer").startup(function(use)
 	use 'kyazdani42/nvim-web-devicons'
 	use 'akinsho/bufferline.nvim'
 	use 'windwp/nvim-autopairs'
-	use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 	use 'windwp/nvim-ts-autotag'
 	use 'lewis6991/gitsigns.nvim'
 	use 'folke/trouble.nvim'
+	use 'nvim-lualine/lualine.nvim'
 
 	use 'hrsh7th/vim-vsnip'
 	use 'hrsh7th/cmp-vsnip'
 
+	use 'williamboman/mason.nvim'
+	use 'williamboman/mason-lspconfig.nvim'
 	use 'neovim/nvim-lspconfig'
+
 	use 'hrsh7th/cmp-nvim-lsp'
 	use 'hrsh7th/cmp-buffer'
 	use 'hrsh7th/cmp-path'
@@ -22,14 +26,28 @@ require("packer").startup(function(use)
 	use 'hrsh7th/nvim-cmp'
 
 	use 'mfussenegger/nvim-dap'
+	use 'jose-elias-alvarez/null-ls.nvim'
+	use 'folke/which-key.nvim'
 end)
+
 
 
 require("bufferline").setup({})
 require("nvim-autopairs").setup({})
 require("nvim-ts-autotag").setup({})
 require("gitsigns").setup({})
-require("trouble").setup({})
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("null-ls").setup()
+require("lualine").setup({})
+require("which-key").setup({})
+
+require("trouble").setup({
+	auto_open = true,
+	auto_close = true,
+	height = 5
+})
+
 
 require("nvim-treesitter.configs").setup({
 	highlight = {
@@ -44,4 +62,3 @@ require("nvim-treesitter.configs").setup({
 		"lua", "c", "rust", "tsx", "javascript", "typescript", "html", "css", "yaml", "toml", "json",
 	}
 })
-
